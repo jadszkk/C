ff9a4110e69dd758ef4e32c1ce9e050caa25d348
jadszkk <jadszcarlos923@gmail.com>
2025-04-11 17:35:56 -0300

    Funções implementadas ao código

--- Atividades/c++-atividade.cpp
+++ Atividades/c++-atividade.cpp
@@ -10,48 +10,33 @@ struct funcionarios{
     char cargo[15];
 };
 
+funcionarios calcularsalario(){
+    funcionarios dados;
+    cout << "Informe o cargo: " << endl;
+    cin >> dados.cargo;
+    cout << "Informe o salário: " << endl;
+    cin >> dados.salario;
 
-int main()
-{
-    //float salario;
-    //float aumentosal;
-    //float salarionovo;
-    //char cargo[15];
-    char opc[5]="sim";
-    int cont=0;
-    funcionarios dados[10];
+    if (strcmp(dados.cargo,"gerente")==0){
+        dados.aumentosal = 0.1*dados.salario;
+        dados.salarionovo = dados.salario+dados.aumentosal;
+    } else if(strcmp(dados.cargo,"engenheiro")==0){
+        dados.aumentosal = 0.2*dados.salario;
+        dados.salarionovo = dados.salario+dados.aumentosal;
+    } else{
+        dados.aumentosal = 0.4*dados.salario;
+        dados.salarionovo = dados.salario+dados.aumentosal;
+    }
+    return dados;
+}
+
+void exibirfuncionarios(funcionarios dados[10], int cont){
+    
     int quantidadeg=0;
     int quantidadee=0;
     float total=0;
     float totala=0;
-    int qnt;
     
-    do{
-    //while(strcmp(opc,"sim")==0){
-    //cout << "Digite a quantidade de funcionarios: " << qnt;
-    //for(int a=0;a<qnt;a++){
-    cout << "Informe o cargo: " << endl;
-    cin >> dados[cont].cargo;
-    cout << "Informe o salário: " << endl;
-    cin >> dados[cont].salario;
-
-    if (strcmp(dados[cont].cargo,"gerente")==0){
-        dados[cont].aumentosal = 0.1*dados[cont].salario;
-        dados[cont].salarionovo = dados[cont].salario+dados[cont].aumentosal;
-    } else if(strcmp(dados[cont].cargo,"engenheiro")==0){
-        dados[cont].aumentosal = 0.2*dados[cont].salario;
-        dados[cont].salarionovo = dados[cont].salario+dados[cont].aumentosal;
-    } else{
-        dados[cont].aumentosal = 0.4*dados[cont].salario;
-        dados[cont].salarionovo = dados[cont].salario+dados[cont].aumentosal;
-    }
-    cont++;
-    //cout << "Salario novo: " << dados[cont].salarionovo << endl << "Salario antigo: " << dados[cont].salario << endl << "Diferença: " << dados[cont].aumentosal << endl;
-    cout << "Deseja realizar a operação novamente?";
-    cin >> opc;
-    cout << endl;
-    //}
-    }while(strcmp(opc,"sim")==0);
     for (int i=0;i<cont;i++){
         cout << "   Funcionário " << i+1 << endl << "Salario novo: " << dados[i].salarionovo << endl << "Salario antigo: " << dados[i].salario << endl << "Diferença: " << dados[i].aumentosal << endl;
         cout << endl;
@@ -65,6 +50,22 @@ int main()
     }
     cout << "Quantidade de gerentes: " << quantidadeg << endl << "Quantidade de engenheiros: " << quantidadee << endl;
     cout << "Custo total antes do aumento: " << total << endl << "Custo total depois do aumento: " << totala;
-    return 0;
 }
 
+int main()
+{
+    
+    char opc[5]="sim";
+    int cont=0;
+    funcionarios dados[10];
+    
+    do{
+    dados[cont]=calcularsalario();
+    cont++;
+    cout << "Deseja realizar a operação novamente?";
+    cin >> opc;
+    cout << endl;
+    }while(strcmp(opc,"sim")==0);
+    exibirfuncionarios(dados,cont);
+    return 0;
+}
