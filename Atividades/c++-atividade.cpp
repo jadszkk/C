e8b136bd4201a027913f0019bb62e25fddaa143a
jadsz <141606075+jadszkk@users.noreply.github.com>
2025-04-27 01:24:25 -0300

    Update c++-atividade.cpp

--- Atividades/c++-atividade.cpp
+++ Atividades/c++-atividade.cpp
@@ -3,69 +3,84 @@
 #include <string.h>
 using namespace std;
 
-struct funcionarios{
-    float salario;
-    float aumentosal;
-    float salarionovo;
-    char cargo[15];
+class funcionarios{
+	private:
+    	float salario, salarionovo, aumentosal;
+    	string cargo;
+    	string nome;
+	public:
+    	void leituradedados();
+    	void calcularsalario();
+    	void exibirfuncionarios();
+    	string getcargo() {return cargo;};
+    	float getsalario() {return salario;};
+    	float getsalarionovo() {return salarionovo;};
 };
 
-funcionarios calcularsalario(){
-    funcionarios dados;
+void funcionarios::leituradedados(){
     cout << "Informe o cargo: " << endl;
-    cin >> dados.cargo;
+    cin >> cargo;
     cout << "Informe o salário: " << endl;
-    cin >> dados.salario;
+    cin >> salario;
+    cout << "Informe o nome: " << endl;
+    cin >> nome;
+};
 
-    if (strcmp(dados.cargo,"gerente")==0){
-        dados.aumentosal = 0.1*dados.salario;
-        dados.salarionovo = dados.salario+dados.aumentosal;
-    } else if(strcmp(dados.cargo,"engenheiro")==0){
-        dados.aumentosal = 0.2*dados.salario;
-        dados.salarionovo = dados.salario+dados.aumentosal;
-    } else{
-        dados.aumentosal = 0.4*dados.salario;
-        dados.salarionovo = dados.salario+dados.aumentosal;
-    }
-    return dados;
-}
-
-void exibirfuncionarios(funcionarios dados[10], int cont){
+void funcionarios::calcularsalario(){
     
+    if (cargo=="gerente"){
+        aumentosal = 0.1*salario;
+    } else if(cargo=="engenheiro"){
+        aumentosal = 0.2*salario;
+    } else{
+        aumentosal = 0.4*salario;
+    }
+    salarionovo = salario+aumentosal;
+};
+
+
+void funcionarios::exibirfuncionarios(){
+    
+    cout << "Nome: " << nome << endl << "Cargo: " << cargo << endl << "Salario novo: " << salarionovo << endl << "Salario antigo: " << salario << endl << "Diferença: " << aumentosal << endl;
+    cout << endl;
+    
+};
+
+int main()
+{
+    string opc="sim";
+    int cont=0;
+    funcionarios dados[10];
     int quantidadeg=0;
     int quantidadee=0;
     float total=0;
     float totala=0;
     
-    for (int i=0;i<cont;i++){
-        cout << "   Funcionário " << i+1 << endl << "Salario novo: " << dados[i].salarionovo << endl << "Salario antigo: " << dados[i].salario << endl << "Diferença: " << dados[i].aumentosal << endl;
-        cout << endl;
-        if(strcmp(dados[i].cargo,"gerente")==0){
-            quantidadeg= quantidadeg+1;
-        } else if (strcmp(dados[i].cargo,"engenheiro")==0){
-            quantidadee= quantidadee+1;
-        }
-        total=total+dados[i].salario;
-        totala=totala+dados[i].salarionovo;
-    }
-    cout << "Quantidade de gerentes: " << quantidadeg << endl << "Quantidade de engenheiros: " << quantidadee << endl;
-    cout << "Custo total antes do aumento: " << total << endl << "Custo total depois do aumento: " << totala;
-}
-
-int main()
-{
-    
-    char opc[5]="sim";
-    int cont=0;
-    funcionarios dados[10];
-    
     do{
-    dados[cont]=calcularsalario();
+    dados[cont].leituradedados();
+    dados[cont].calcularsalario();
     cont++;
     cout << "Deseja realizar a operação novamente?";
     cin >> opc;
     cout << endl;
-    }while(strcmp(opc,"sim")==0);
-    exibirfuncionarios(dados,cont);
+    }while(opc=="sim");
+    
+    for (int i=0; i<cont; i++){
+        cout << "Funcionário " << i + 1 << endl;
+        dados[i].exibirfuncionarios();
+        
+        if(dados[i].getcargo()=="gerente"){
+        quantidadeg=quantidadeg + 1;
+        } else if (dados[i].getcargo()=="engenheiro"){
+            quantidadee=quantidadee + 1;
+        }
+        
+        total=total+dados[i].getsalario();
+        totala=totala+dados[i].getsalarionovo();
+    }
+    
+    cout << "Quantidade de gerentes: " << quantidadeg << endl << "Quantidade de engenheiros: " << quantidadee << endl;
+    cout << "Custo total antes do aumento: " << total << endl << "Custo total depois do aumento: " << totala;
+    
     return 0;
 }
